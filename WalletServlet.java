82a80636d2369ccbc016f695d0aa34b8e6b93298
fixercoin <khanbabusargodhaa@gmail.com>
2025-07-04 01:13:43 +0500

    Update and rename login.php to WalletServlet.java

---
+++ WalletServlet.java
@@ -0,0 +1,10 @@
+ // WalletServlet.java
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java
